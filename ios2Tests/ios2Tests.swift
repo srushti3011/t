0526fbc6025a905d029a941e2522730456275682
Srushti Sarju Engineer <srushti.engineer@simformsolutions.com>
2025-01-16 17:10:19 +0530

    Initial Commit

---
+++ ios2Tests/ios2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ios2Tests.swift
+//  ios2Tests
+//
+//  Created by Srushti Sarju Engineer on 16/01/25.
+//
+
+import Testing
+@testable import ios2
+
+struct ios2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
